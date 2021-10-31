2b75c25f096bc322160f22ef0333d1669aa6ea09
Gilvenf <91202566+Gilvenf@users.noreply.github.com>
2021-10-31 22:22:23 +0700

    Add files via upload

---
+++ Prak5Elkom1.py
@@ -0,0 +1,11 @@
+print("--------------DERET FIBONACCI----------------")
+jumlahbilangan =int(input("Masukkan jumlah bilangan:"))
+bilangan1 =int(input("Bilangan Pertama:"))
+bilangan2 =int(input("Bilangan Kedua:"))
+bilangan3 =0,1,0
+for i in range (jumlahbilangan):     
+    print(bilangan1)    
+    bilangan3=bilangan1+bilangan2
+    bilangan1=bilangan2  
+    bilangan2=bilangan3 
+    
